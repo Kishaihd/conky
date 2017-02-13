215afc584831c3b2dcd6eb72514cae0a32616cc4
Kishaihd <mr.pinstripe_suit@hotmail.com>
2017-02-13 09:41:50 -0700

    Updated scripts, added files, lots of stuff.

---
+++ conkill.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+c1=$(ps -e | grep conky | grep -o "[0-9][0-9][0-9][0-9][0-9]*" | head -n 1)
+
+c2=$(ps -e | grep conky | grep -o "[0-9][0-9][0-9][0-9][0-9]*" | tail -n 1)
+
+echo "Conky PID 1: $c1"
+echo "Conky PID 2: $c2"
+
+echo "Killing Conky processes..."
+
+kill $c1
+kill $c2
+
+sleep 5
+
+echo "Done."
+
